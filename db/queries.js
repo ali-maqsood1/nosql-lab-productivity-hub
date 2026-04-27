57c52ede9a96b0e2b7389cbf63bd2add4488535d
Ali Maqsood <alimaqsood35892@gmail.com>
2026-04-27 07:05:35 +0500

    Implement query 8

--- db/queries.js
+++ db/queries.js
@@ -209,8 +209,11 @@ async function createTask(db, taskData) {
  * Hint: updateOne + $set.
  */
 async function updateTaskStatus(db, taskId, newStatus) {
-  // TODO: implement
-  throw new Error('updateTaskStatus not implemented');
+  const result = await db.collection('tasks').updateOne(
+    { _id: taskId },
+    { $set: { status: newStatus } }
+  );
+  return { matchedCount: result.matchedCount, modifiedCount: result.modifiedCount };
 }
 
 /**
