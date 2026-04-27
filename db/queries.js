db1f86ae634129cbb426a02a85c215565188c5cf
Ali Maqsood <alimaqsood35892@gmail.com>
2026-04-27 08:33:07 +0500

    Implemenet query 15

--- db/queries.js
+++ db/queries.js
@@ -431,8 +431,27 @@ async function projectTaskSummary(db, ownerId) {
  *       you only want to look up 10 projects, not all of them.
  */
 async function recentActivityFeed(db, ownerId) {
-  // TODO: implement
-  throw new Error('recentActivityFeed not implemented');
+  return db.collection('tasks').aggregate([
+    { $match: { ownerId } },
+    { $sort: { createdAt: -1 } },
+    { $limit: 10 },
+    { $lookup: {
+        from: 'projects',
+        localField: 'projectId',
+        foreignField: '_id',
+        as: 'project'
+    }},
+    { $unwind: '$project' },
+    { $project: {
+        _id: 1,
+        title: 1,
+        status: 1,
+        priority: 1,
+        createdAt: 1,
+        projectId: 1,
+        projectName: '$project.name'
+    }}
+  ]).toArray();
 }
 
 // =============================================================================
