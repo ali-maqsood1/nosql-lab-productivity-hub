103bf1be5e4713212bebde6c6c6aaf53a2d25535
Ali Maqsood <alimaqsood35892@gmail.com>
2026-04-27 07:07:16 +0500

    Implement query 11

--- db/queries.js
+++ db/queries.js
@@ -290,8 +290,11 @@ async function removeTaskTag(db, taskId, tag) {
  *       matched), and your $set path uses `subtasks.$.done`.
  */
 async function toggleSubtask(db, taskId, subtaskTitle, newDone) {
-  // TODO: implement
-  throw new Error('toggleSubtask not implemented');
+  const result = await db.collection('tasks').updateOne(
+    { _id: taskId, 'subtasks.title': subtaskTitle },
+    { $set: { 'subtasks.$.done': newDone } }
+  );
+  return { matchedCount: result.matchedCount, modifiedCount: result.modifiedCount };
 }
 
 /**
