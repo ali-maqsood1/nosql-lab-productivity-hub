fc7f266e7c9a2c33d02f1193421a00cab3ff49dc
Ali Maqsood <alimaqsood35892@gmail.com>
2026-04-27 07:01:15 +0500

    Implement query 5

--- db/queries.js
+++ db/queries.js
@@ -130,8 +130,11 @@ async function createProject(db, projectData) {
  * Hint: updateOne with the $set operator.
  */
 async function archiveProject(db, projectId) {
-  // TODO: implement
-  throw new Error('archiveProject not implemented');
+  const result = await db.collection('projects').updateOne(
+    { _id: projectId },
+    { $set: { archived: true } }
+  );
+  return { matchedCount: result.matchedCount, modifiedCount: result.modifiedCount };
 }
 
 /**
